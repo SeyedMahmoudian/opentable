28e5b50ea3b7f455f076782a55ef0877cff18112
SeyedMahmoudian <amin.s.mahmoudian@gmail.com>
2020-05-09 22:58:17 -0400

    test server

--- src/Forms.js
+++ src/Forms.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-
+// import axios from 'axios';
 
 class Forms extends React.Component {
     state = { dest: '' };
@@ -38,7 +38,7 @@ class Forms extends React.Component {
         const { error, isLoaded, cities } = this.state;
 
         if (error) {
-            return <div className="row"><div className="col-lg-4 col-md-4 col-xs-12 col-sm-12"><img src="/pics/loading.gif" alt="loading.." /></div></div>
+            return <div className="row"><div className="col-lg-4 col-md-4 col-xs-12 col-sm-12"><img src="./pics/loading.gif" alt="loading.." /></div></div>
         } else if (!isLoaded) {
             return <div className="row"><div className="col-lg-4 col-md-4 col-xs-12 col-sm-12"><p>Error!</p></div></div>
         } else {
